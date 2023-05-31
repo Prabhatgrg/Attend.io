b2e4371377f68114d1b882ac6d364cb83d969b0e
Prabhatgrg <prabhatgurung34@gmail.com>
2023-05-31 08:50:24 +0545

    Started Login Check

--- backend/login.php
+++ backend/login.php
@@ -1,5 +1,19 @@
 <?php
 
+require_once('../database/config.php');
 
+//Check form submission
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //Check if credential match with database
+    $query = "SELECT * FROM admin WHERE username=$username";
+    $result = mysqli_query($con, $query);
+    
+    if(mysqli_num_rows($result)){
+
+    }
+}
 
 ?>
