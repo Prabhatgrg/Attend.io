4575a165ac268b736b962742d607dc0416b96501
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-04 22:32:55 +0545

    Logout

--- dashboard.php
+++ dashboard.php
@@ -20,7 +20,7 @@
       <h3 class="text-center">Admin Dashboard</h3>
 
       <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Logout</a></li>
+        <li class="nav-item"><a href="./backend/logout.php" class="nav-link active" aria-current="page">Logout</a></li>
       </ul>
     </header>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
