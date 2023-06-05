6fc326e1664ac009cd697a59a69160537704c438
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-05 18:53:47 +0545

    Updated Schema with attendance and student table

--- database/schema.sql
+++ database/schema.sql
@@ -8,6 +8,13 @@ CREATE TABLE IF NOT EXISTS admin(
 CREATE TABLE IF NOT EXISTS students(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL, 
-    semester VARCHAR(10) NOT NULL,
-    status ENUM('present', 'absent', 'on_leave')
+    semester VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS attendance(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    status ENUM('present', 'absent', 'on_leave'),
+    currentDate date NOT NULL DEFAULT CURRENT_DATE(),
+    FOREIGN KEY (student_id) REFERENCES students(id)
 );
