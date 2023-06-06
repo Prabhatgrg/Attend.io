20992450721a6da6def9185a5dac6ab04f8edbf3
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-06 17:26:13 +0545

    Successfully added Date values as well

--- backend/attendance.php
+++ backend/attendance.php
@@ -8,12 +8,11 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
     $studentName = $_POST['studentName'];
     $semester = $_POST['semester'];
     $status = $_POST['status'];
-    $getDate = date('Y-m-d');
-    $currentDate = date('Y-m-d H:i:s');
+    $currentDate = date('Y-m-d');
     // $currentDate = CURRENT_DATE();
     // $query = "INSERT INTO attendance(student_id, status, currentDate)VALUES()";
     $query = $conn->prepare("INSERT INTO attendance(student_name, semester, status, currentDate)VALUES(?,?,?, ?)");
-    $query->bind_param("sisi", $studentName, $semester, $status, $currentDate);
+    $query->bind_param("siss", $studentName, $semester, $status, $currentDate);
 
     if($query->execute()){
         $_SESSION['success'] = "Data Inserted Successfully";
