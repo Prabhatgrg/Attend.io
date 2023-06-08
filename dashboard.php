bcb496fd878d3d2983be86c747fc0fd0c435bb9b
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-08 22:33:28 +0545

    Trying to populate table with database data. Need more work

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,6 @@
-<?php session_start() ?>
+<?php
+require_once './database/config.php';
+session_start() ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,10 +28,10 @@
     </ul>
   </header>
   <?php
-    if(isset($_SESSION['status'])){
-      echo "<h3>" . $_SESSION['status'] . "<br>";
-      unset($_SESSION['status']);
-    }
+  if (isset($_SESSION['status'])) {
+    echo "<h3>" . $_SESSION['status'] . "<br>";
+    unset($_SESSION['status']);
+  }
   ?>
   <div class="form-control">
     <form action="./backend/attendance.php" method="post" class="d-flex gap-4 justify-content-center align-items-center">
@@ -50,7 +52,7 @@
       <div class="info">
         <input type="text" placeholder="Student Name" name="studentName">
       </div>
-      
+
       <input type="radio" class="btn-check" name="status" id="present" value="present" autocomplete="off" checked>
       <label class="btn btn-outline-success" for="present">present</label>
 
@@ -59,19 +61,136 @@
 
       <input type="radio" class="btn-check" name="status" id="on-leave" value="on-leave" autocomplete="off">
       <label class="btn btn-outline-warning" for="on-leave">on-leave</label>
-      
+
       <button type="submit" class="btn btn-dark" name="add">Add</button>
     </form>
   </div>
 
-  <?php while($row>0){?>
-    <table>
-      <th>Name</th>
-      <th>Semester</th>
-      <th>Status</th>
-      <th>Update</th>
-    </table>
+  <?php
+  $query = "SELECT * FROM attendance";
+  $result = mysqli_query($conn, $query);
+  $numRows = mysqli_num_rows($result);
 
+  while ($numRows > 0) {
+    for ($i = 1; $i <= $numRows; $i++) { ?>
+      <h3 class="text-center">Date Goes Here</h3>
+      <table class="table table-dark table-hover text-center">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Semester</th>
+            <th scope="col">Status</th>
+            <th scope="col">Update</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td class=" ps-5 w-25">
+              <button class="btn bg-success">present</button>
+              <button class="btn bg-danger">absent</button>
+              <button class="btn bg-warning">on-leave</button>
+            </td>
+            <td class=" w-25">
+              <button class="btn bg-success">Edit</button>
+              <button class="btn bg-danger">Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td class=" ps-5 w-25">
+              <button class="btn bg-success">present</button>
+              <button class="btn bg-danger">absent</button>
+              <button class="btn bg-warning">on-leave</button>
+            </td>
+            <td class=" w-25">
+              <button class="btn bg-success">Edit</button>
+              <button class="btn bg-danger">Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td class=" ps-5 w-25">
+              <button class="btn bg-success">present</button>
+              <button class="btn bg-danger">absent</button>
+              <button class="btn bg-warning">on-leave</button>
+            </td>
+            <td class=" w-25">
+              <button class="btn bg-success">Edit</button>
+              <button class="btn bg-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  <?php
+    }
+    break;
+  } ?>
+
+
+  <!-- <table class="table table-hover table-striped table-bordered table-dark text-center">
+    <h3 class="text-center">Date Goes Here</h3>
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Semester</th>
+        <th scope="col">Status</th>
+        <th scope="col">Update</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td class=" ps-5 w-25">
+          <button class="btn bg-success">present</button>
+          <button class="btn bg-danger">absent</button>
+          <button class="btn bg-warning">on-leave</button>
+        </td>
+        <td class=" w-25">
+          <button class="btn bg-success">Edit</button>
+          <button class="btn bg-danger">Delete</button>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td class=" ps-5 w-25">
+          <button class="btn bg-success">present</button>
+          <button class="btn bg-danger">absent</button>
+          <button class="btn bg-warning">on-leave</button>
+        </td>
+        <td class=" w-25">
+          <button class="btn bg-success">Edit</button>
+          <button class="btn bg-danger">Delete</button>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td class=" ps-5 w-25">
+          <button class="btn bg-success">present</button>
+          <button class="btn bg-danger">absent</button>
+          <button class="btn bg-warning">on-leave</button>
+        </td>
+        <td class=" w-25">
+          <button class="btn bg-success">Edit</button>
+          <button class="btn bg-danger">Delete</button>
+        </td>
+      </tr>
+    </tbody>
+  </table> -->
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
