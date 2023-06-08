8bcec4acf6ab263fdabb4d3e3fdf285b6c7242dc
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-08 20:49:30 +0545

    Read Data from database started

--- dashboard.php
+++ dashboard.php
@@ -64,7 +64,14 @@
     </form>
   </div>
 
-  <?php while()?>
+  <?php while($row>0){?>
+    <table>
+      <th>Name</th>
+      <th>Semester</th>
+      <th>Status</th>
+      <th>Update</th>
+    </table>
+
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
