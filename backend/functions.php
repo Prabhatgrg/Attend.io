ec02085208cfb9d74185d60d253d1eb917d0042b
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-20 20:39:46 +0545

    User Authentication Started

--- backend/functions.php
+++ backend/functions.php
@@ -2,6 +2,45 @@
 
 require_once './database/config.php';
 
+// User Functions
+function is_login(){
+    if(isset($_SESSION['id'])){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function check_if_login(){
+    if(!is_login()){
+        header('Location: ' . './index.php');
+    }
+}
+
+// Authentication
+function auth($username, $password){
+    global $conn;
+
+    $stmt=$conn->prepare("SELECT * FROM admin WHERE username=?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if($result->num_rows==0){
+        echo 'Username does not exist' ;
+    }else{
+        $user=$result->fetch_array(MYSQLI_ASSOC);
+        if($user['username']==$username && password_verify($password,$user['password'])){
+            $_SESSION['id']=$user['id'];
+            $_SESSION['username']=$user['username'];
+            header('Location: ./dashboard.php');
+        }else{
+            echo 'Incorrect username or password';
+        }
+    }
+}
+
 // Query to retrieve the current date from the database
 function retrieveDate(){
     global $conn;
@@ -19,5 +58,3 @@ function retrieveDate(){
         echo "No data found!";
     }
 }
-
-// User Functions
