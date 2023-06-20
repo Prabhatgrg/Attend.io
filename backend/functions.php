9544136f354ff0983e638746651e4c45445fc2ab
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-20 18:50:33 +0545

    Admin/User Function Started

--- backend/functions.php
+++ backend/functions.php
@@ -1,28 +1,6 @@
 <?php
 
 require_once './database/config.php';
-// function retrieveDate()
-// {
-//     global $conn;
-//     $stmt = $conn->prepare("SELECT currentDate FROM attendance");
-//     if($stmt->execute()){
-//         $data = $stmt->get_result();
-//         $unformat = $data->fetch_all(MYSQLI_ASSOC);
-//     }
-// $today_date = date('Y-m-d');
-// }
-// function getDataByDate($data_result){
-//     global $conn;
-//     $stmt = $conn->prepare("SELECT * FROM attendance WHERE date = ?");
-//     $stmt->bind_param("s", $data_result);
-//     if($stmt->execute()){
-//         $result = $stmt->get_result();
-//         $data = $result->fetch_all(MYSQLI_ASSOC);
-//         $today_date = new DateTime('Y-m-d');
-//         return var_dump($today_date);
-//         // return $today_date;
-//     }
-// }
 
 // Query to retrieve the current date from the database
 function retrieveDate(){
@@ -40,5 +18,6 @@ function retrieveDate(){
     } else {
         echo "No data found!";
     }
-    
 }
+
+// User Functions
