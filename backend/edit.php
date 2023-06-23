5d2bc8f40d64c13aa48935884bcb966dd9044ec3
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-23 09:57:25 +0545

    Resolved status issue

--- backend/edit.php
+++ backend/edit.php
@@ -1,14 +1,6 @@
 <?php
 require_once '../database/config.php';
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $status = $_POST['status'];
-    $stmt = $conn->prepare("UPDATE attendance SET status=?");
-    $stmt->bind_param("s", $status);
-    if($stmt->execute()){
-        header('Location: ../dashboard.php');
-    }
-}
 
 if(!isset($_GET['attendance_id'])){
     header('Location: ../dashboard.php');
@@ -18,7 +10,16 @@ $attendance_id = $_GET['attendance_id'];
 echo $attendance_id;
 
 $stmt=$conn->prepare("SELECT status FROM attendance WHERE attendance_id=?");
-$stmt->bind_param("s", $attendance_id);
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $status = $_POST['status'];
+    $stmt = $conn->prepare("UPDATE attendance SET status=? WHERE attendance_id=?");
+    $stmt->bind_param("si", $status, $attendance_id);
+    if($stmt->execute()){
+        header('Location: ../dashboard.php');
+    }
+}
+
+$stmt->bind_param("i", $attendance_id);
 $stmt->execute();
 
 $result = $stmt->get_result();
