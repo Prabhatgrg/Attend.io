bbf8fa61e2ce402ef08cc8a656091fa475571c5f
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-23 19:21:34 +0545

    Updated edit.php file

--- backend/edit.php
+++ backend/edit.php
@@ -2,19 +2,19 @@
 require_once '../database/config.php';
 
 
-if(!isset($_GET['attendance_id'])){
+if (!isset($_GET['attendance_id'])) {
     header('Location: ../dashboard.php');
 }
 
 $attendance_id = $_GET['attendance_id'];
 echo $attendance_id;
 
-$stmt=$conn->prepare("SELECT status FROM attendance WHERE attendance_id=?");
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+$stmt = $conn->prepare("SELECT status FROM attendance WHERE attendance_id=?");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $status = $_POST['status'];
     $stmt = $conn->prepare("UPDATE attendance SET status=? WHERE attendance_id=?");
     $stmt->bind_param("si", $status, $attendance_id);
-    if($stmt->execute()){
+    if ($stmt->execute()) {
         header('Location: ../dashboard.php');
     }
 }
@@ -24,13 +24,33 @@ $stmt->execute();
 
 $result = $stmt->get_result();
 $row = $result->fetch_array(MYSQLI_ASSOC)['status'];
+// $row = $result->fetch_array(MYSQLI_ASSOC);
+// var_dump($row);
 ?>
 
-<form method="post">
-    <select name="status" id="">
-        <option value="present" <?php if ($row == 'present') echo 'selected'; ?>>present</option>
-        <option value="absent" <?php if ($row == 'absent') echo 'selected'; ?>>absent</option>
-        <option value="on-leave" <?php if ($row == 'on-leave') echo 'selected'; ?>>on-leave</option>
-    </select>
-    <button type="submit">Submit</button>
-</form>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+
+    <div class="form-control">
+        <a href="../dashboard.php" type="button" class="btn btn-success">Back</a>
+        <form method="post" class="d-flex flex-column gap-4 justify-content-center align-items-center">
+            <div class="d-flex align-items-center title">
+                <h3>Edit Status for Attendance ID: <?php echo $attendance_id; ?></h3>
+
+            </div>
+            <select name="status" id="">
+                <option value="present" <?php if ($row == 'present') echo 'selected'; ?>>present</option>
+                <option value="absent" <?php if ($row == 'absent') echo 'selected'; ?>>absent</option>
+                <option value="on-leave" <?php if ($row == 'on-leave') echo 'selected'; ?>>on-leave</option>
+            </select>
+            <button type="submit" class="btn btn-dark">Submit</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
