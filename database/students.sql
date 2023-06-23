60ecd92b04aaec9dbe3cfa90b3db66f46f492756
Prabhatgrg <prabhatgurung34@gmail.com>
2023-06-23 19:00:57 +0545

    Added students.sql file

--- database/students.sql
+++ database/students.sql
@@ -1,19 +1,18 @@
 INSERT INTO students(name)
-VALUES(
-    1,'Prabhat Gurung',
-    2,'Neer Bahadur Shrestha',
-    3,'Pujan Tandukar',
-    4,'Pawon Shrestha',
-    5,'Aarik Maharjan',
-    6,'Hemant Karki',
-    7,'Subash Shrestha',
-    8,'Ram Krishna Shrestha',
-    9,'Sujan Maharjan',
-    10,'Sandip Maharjan',
-    11,'Aayush Shahi',
-    12,'Rojina Maharjan',
-    13,'Rejina Pokharel',
-    14,'Rojin Shrestha',
-    15,'Nischal Maharjan',
-    16,'Devashish Gole'
-);
+VALUES
+    (1,'Prabhat Gurung'),
+    (2,'Neer Bahadur Shrestha'),
+    (3,'Pujan Tandukar'),
+    (4,'Pawon Shrestha'),
+    (5,'Aarik Maharjan'),
+    (6,'Hemant Karki'),
+    (7,'Subash Shrestha'),
+    (8,'Ram Krishna Shrestha'),
+    (9,'Sujan Maharjan'),
+    (10,'Sandip Maharjan'),
+    (11,'Aayush Shahi'),
+    (12,'Rojina Maharjan'),
+    (13,'Rejina Pokharel'),
+    (14,'Rojin Shrestha'),
+    (15,'Nischal Maharjan'),
+    (16,'Devashish Gole');
